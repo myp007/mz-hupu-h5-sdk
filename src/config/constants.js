2a44aa7fa531f7e26e299336791a498a3eabe7e1
myp007 <1194770343@qq.com>
2025-12-01 17:31:17 +0800

    修改key

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 // 游戏配置常量
 export const GAME_CONFIG = {
   GAME_ID: '2661', // 统一游戏ID
-  GAME_KEY: '1af869688184a4b40cf078ee52a5dc6d',
+  GAME_KEY: 'JR7EjeKU3f8n5JxiuGgGOKnhUIBfAHg8',
   GAME_VERSION: '1.0',
   SDK_VERSION: '6.1.0'
 }
